638a72e7aa6ca559528929895491f305d8ccc95d
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-27 23:39:32 +0900

    Update main.js

--- code/js/main.js
+++ code/js/main.js
@@ -1,3 +1,16 @@
+//notice
+
+var desc = new XMLHttpRequest();
+desc.open('GET', 'https://gk219.kro.kr/code/json/notice.json', true);
+desc.onload = function () {
+    var data = JSON.parse(this.response);
+    data.forEach((type) => {
+        document.getElementById('notice').innerHTML += type.replace('/n/', '<br/>');
+    })
+}
+desc.send();
+
+
 //title
 
 document.getElementById('title').innerHTML = '<b>grand kiwi</b>';
