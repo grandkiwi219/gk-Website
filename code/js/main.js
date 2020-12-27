707fe3540b36adaf3a4bdaf8ca2a8fe28dfe4666
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-27 23:00:44 +0900

    Update main.js

--- code/js/main.js
+++ code/js/main.js
@@ -29,6 +29,14 @@ menu_type.forEach(desc => menu_description += desc);
 
 if (title == 'Home') {
     document.getElementById('menu').innerHTML = menu_description.replace('<a href="/">Home</a>', '<a class="here" href="/"><b>Home</b></a>');
+} else if (title == 'Information') {
+    document.getElementById('menu').innerHTML = menu_description.replace('<a href="/information">Information</a>', '<a class="here" href="/information"><b>Information</b></a>');
+} else if (title == 'Community') {
+    document.getElementById('menu').innerHTML = menu_description.replace('<a href="/community">Community</a>', '<a class="here" href="/community"><b>Community</b></a>');
+} else if (title == 'Project') {
+    document.getElementById('menu').innerHTML = menu_description.replace('<a href="/project">Project</a>', '<a class="here" href="/project"><b>Project</b></a>');
+} else if (title == 'Status') {
+    document.getElementById('menu').innerHTML = menu_description.replace('<a href="/status">Status</a>', '<a class="here" href="/status"><b>Status</b></a>');
 } else {
     document.getElementById('menu').innerHTML = menu_description;
 }
