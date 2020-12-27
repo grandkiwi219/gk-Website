42b8c48bd538f35db9b6108aa46fc6ed6fb17429
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-28 08:51:27 +0900

    Update main.js

--- code/js/main.js
+++ code/js/main.js
@@ -4,10 +4,11 @@ var desc = new XMLHttpRequest();
 desc.open('GET', 'https://gk219.kro.kr/code/json/notice.json', true);
 desc.onload = function () {
     var data = JSON.parse(this.response);
-    if (data[0] == '') $('#notice').remove('#notice')
-    data.forEach((noticeType) => {
-        document.getElementById('notice').innerHTML += noticeType.replace('/n/', '<br/>');
-    })
+    if (data[0] == '') { $('#notice').remove('#notice') } else {
+        data.forEach((noticeType) => {
+            document.getElementById('notice').innerHTML += noticeType.replace('/n/', '<br/>');
+        })
+    }
 }
 desc.send();
 
