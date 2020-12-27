d831eb75de8c10725c16ae4c330447ee26266b8a
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-27 20:53:15 +0900

    Update main.js

--- code/js/main.js
+++ code/js/main.js
@@ -1,1 +1,10 @@
-
+$( document ).ready( function() {
+  var menu = $( '#menu' ).offset();
+  $( window ).scroll( function() {
+    if ( $( document ).scrollTop() > menu.top ) {
+      $( '#menu' ).addClass( 'fixed' );
+    } else {
+      $( '#menu' ).removeClass( 'fixed' );
+    }
+  });
+} );
