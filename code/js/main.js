b0d703a7cd81b1ce9d0a554233342432edc1c433
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-27 22:55:35 +0900

    Update main.js

--- code/js/main.js
+++ code/js/main.js
@@ -11,7 +11,7 @@ $( document ).ready( function() {
   } );
 } );
 
-const title = document.title.replace('grand kiwi | ', '').replace('grand kiwi test | ', '');
+const title = document.title.replace('grand kiwi | ', '');
 
 var menu_description = '';
 
