55081d700b7f3d8a845601e369fc8102bd0e4f44
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-28 09:18:54 +0900

    Update main.js

--- code/js/main.js
+++ code/js/main.js
@@ -1,16 +1,13 @@
 //notice
 
-var desc = new XMLHttpRequest();
-desc.open('GET', 'https://gk219.kro.kr/code/json/notice.json', true);
-desc.onload = function () {
-    var data = JSON.parse(this.response);
-    if (data[0] == '') { $('#notice').remove('#notice') } else {
-        data.forEach((noticeType) => {
-            document.getElementById('notice').innerHTML += noticeType.replace('/n/', '<br/>');
-        })
+var notice = '';
+
+    if (notice = '') {
+        console.log('공지 없음')
+        $('#notice').remove('#notice')
+    } else {
+        document.getElementById('notice').innerHtml = notice;
     }
-}
-desc.send();
 
 
 //title
